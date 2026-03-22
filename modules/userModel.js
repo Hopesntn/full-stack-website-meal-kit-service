d9daa46088a5301ae54d1357d566ae60f2ef349a
Hopesntn <fabri.ortiz2005@hotmail.com>
2026-03-21 22:05:38 -0400

    Log-in and cart logic for customer finished

--- modules/userModel.js
+++ modules/userModel.js
@@ -1,10 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
     firstName: { type: String, required: true },
     lastName: { type: String, required: true },
     email: { type: String, required: true, unique: true },
-    password: { type: String, required: true }
+    password: { type: String, required: true },
+    role: {type: String, default: "customer"}
 });
 
-module.exports = mongoose.model('User', userSchema);
+
+// has for passwords (do not use arrow function )
+userSchema.pre("save", async function (next) {
+    const user = this;
+
+    // don't change pass if already hashed
+    if(!user.isModified('password')) {
+        return;
+    }
+
+    try {
+        const salt = await bcrypt.genSalt(10);
+        user.password = await bcrypt.hash(user.password, salt);
+    }
+    //generate a unique SALT
+    // when you generate the hash use 10 rounds
+    catch (err) {
+        throw new Error(`Hashing Failed: ${err.message}`);
+    }
+});
+
+const nameModel = mongoose.model("Users", userSchema);
+module.exports = {nameModel};
